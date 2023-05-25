52bf3e3f300d93e71b87b4bee6416955a1864487
HimanshuJha <jha.himanshu966@gmail.com>
2023-05-25 22:45:52 +0530

    Create Blog and Get Blog Apis Created and modified

---
+++ controller/blogController.js
@@ -0,0 +1,21 @@
+const authorModel = require("../model/authorModel");
+const createAuthor = async function (req, res) {
+  const author = req.body;
+  const authorEmail = req.body.email;
+  const availAuthor = await authorModel.findOne({ email: authorEmail });
+  if (availAuthor) {
+    res.send("Author is already exist");
+  } else {
+    const createdAuthor = await authorModel.create(author);
+    res.status(201).send({ status: true, data: createdAuthor });
+  }
+};
+const getAuthor = async function (req, res) {
+  const author = await authorModel.find();
+  if (!author) {
+    res.status(404).send("No author available");
+  } else {
+    res.send({ status: true, data: author });
+  }
+};
+module.exports = { createAuthor, getAuthor };
