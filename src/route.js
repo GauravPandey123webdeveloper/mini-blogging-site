d1b9bed718b1a32d91b502c4652340138c848d2e
GauravPandey123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2023-05-25 23:47:44 +0530

    completeAPIs

--- src/route.js
+++ src/route.js
@@ -9,9 +9,7 @@ router.post('/authors',authorController.createAuthor)//himanshu jha
 router.post('/blogs', blogController.createBlog)//rishikesh khare
 router.get('/authors',authorController.getAuthor)//gaurav pandey
 router.get('/blogs',blogController.getBlog)//umme habeeba ansari
-// router.put('/authors',authorController.updateAuthor)
-// router.delete('/authors',authorController.deleteAuthor)
-// router.put('/blog/:blogId',blogController.updateBlog)
-// router.delete('/blogs/:blogId',blogController.updateBlog)
-// router.delete('/blogs',blogController.deleteBlog)
+router.put('/blogs/:blogId',blogController.updateBlog)
+router.delete('/blogs/:blogId',blogController.deleteBlogByParam)
+router.delete('/blogs',blogController.deleteBlogByQuery)
 module.exports=router
