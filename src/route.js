861f4be42e14da67d68d4ee5ec52e4a14b4e616c
GauravPandey123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2023-05-25 12:33:39 +0530

    structureUpdate

--- src/route.js
+++ src/route.js
@@ -2,13 +2,16 @@ const express= require('express')
 const authorController=require('../controller/authorController')
 const blogController=require('../controller/blogController')
 const router=express.Router()
-router.post('/authors',authorController.createAuthor)
-router.post('/blogs', blogController.createBlog)
-router.get('/authors',authorController.getAuthor)
-router.get('/blogs',blogController.getBlog)
-router.put('/authors',authorController.updateAuthor)
-router.delete('/authors',authorController.deleteAuthor)
-router.put('/blog/:blogId',blogController.updateBlog)
-router.delete('/blogs/:blogId',blogController.updateBlog)
-router.delete('/blogs',blogController.deleteBlog)
+router.get('/test', function(req,res){
+    res.send("hello genius")
+})
+// router.post('/authors',authorController.createAuthor)//himanshu jha
+// router.post('/blogs', blogController.createBlog)//rishikesh khare
+// router.get('/authors',authorController.getAuthor)//gaurav pandey
+// router.get('/blogs',blogController.getBlog)//umme habeeba ansari
+// router.put('/authors',authorController.updateAuthor)
+// router.delete('/authors',authorController.deleteAuthor)
+// router.put('/blog/:blogId',blogController.updateBlog)
+// router.delete('/blogs/:blogId',blogController.updateBlog)
+// router.delete('/blogs',blogController.deleteBlog)
 module.exports=router
